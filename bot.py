610114de1d3d3206923dd82dac84f94de8b2fb71
Artem Denisov <artem.denisov@devexpress.com>
2019-08-01 13:16:43 +0300

    add requirements

---
+++ bot.py
@@ -0,0 +1,44 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+def hello(update, context):
+    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
+
+
+def custom(update, context):
+    update.message.reply_text('Custom')
+    context.bot.send_photo(chat_id=update.message.chat_id, photo=open('images/414.png', 'rb'))
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+TOKEN = '668429632:AAHheR0-J4RfL1LYLOtX5nDTHfs4WJJrCWw'
+
+REQUEST_KWARGS={
+    'proxy_url': 'socks5://104.238.187.21:32245',
+    # Optional, if you need authentication:
+#    'urllib3_proxy_kwargs': {
+#        'username': 'PROXY_USER',
+#        'password': 'PROXY_PASS',
+  #  }
+}
+
+updater = Updater(token=TOKEN, use_context=True, request_kwargs=REQUEST_KWARGS)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('custom', custom))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+updater.start_polling()
+updater.idle()
