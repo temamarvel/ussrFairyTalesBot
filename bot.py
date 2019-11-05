4a6a3e2d52608ca93de62591d155b22f6c98f7da
temamarvel <tema.denisoff@gmail.com>
2019-11-05 16:43:53 +0300

    custom chat action

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ def start(update, context):
 
 
 def echo(update, context):
-    context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_AUDIO)
-    context.bot.send_message(chat_id=update.message.chat_id, text='searching...')
+    context.bot.send_chat_action(chat_id=update.message.chat_id, action='searching...')
+    #context.bot.send_message(chat_id=update.message.chat_id, text='searching...')
     # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     # cursor = conn.cursor()
     # context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
