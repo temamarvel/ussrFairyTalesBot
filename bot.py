b505f3a0fe096a54dc8f70c798f65020873eeaef
temamarvel <tema.denisoff@gmail.com>
2019-11-05 16:34:51 +0300

    add nityfication

--- bot.py
+++ bot.py
@@ -28,6 +28,7 @@ def start(update, context):
 
 
 def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='searching...')
     # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     # cursor = conn.cursor()
     # context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
